ba6a5042585028c1a3244a8a199e87322d293346
Aziz | Python Developer <ergashevazizdev@gmail.com>
2025-10-30 23:55:37 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
